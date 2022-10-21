223292a7600d4cf9139fba82c21f4148df11f1fb
Ryan Indrajaya <sryan062@gmail.com>
2022-10-21 15:07:05 +0700

    twitter prototype

---
+++ sanity/schemas/tweet.js
@@ -0,0 +1,34 @@
+export default {
+  name: "tweet",
+  title: "Tweet",
+  type: "document",
+  fields: [
+    {
+      name: "text",
+      title: "Text  in Tweet",
+      type: "string",
+    },
+    {
+      name: "blockTweet",
+      title: "Block Tweet",
+      description:
+        "Hanya untuk ADMIN: Jika tweet tidak sesuai dengan regulasi Tweeter!",
+      type: "boolean",
+    },
+    {
+      name: "username",
+      title: "Username",
+      type: "string",
+    },
+    {
+      name: 'profileImg',
+      title: 'Profile Image',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Tweet Image',
+      type: 'string',
+    },
+  ],
+};
